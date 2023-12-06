6a0252c24afa9bf8d714580c637176b13430b8c6
=Kamil Alparslan Tüfek <kalparslantufek@gmail.com>
2023-12-06 17:44:07 +0300

    update

--- scraper/Controllers/SafebooruController.cs
+++ scraper/Controllers/SafebooruController.cs
@@ -18,8 +18,8 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
-    [HttpGet(Name = "SafebooruImage")]
-    public async Task<String?> Get()
+
+    public async Task<String> maxId()
     {
         // sayfaya giriş yap
         var url = "https://safebooru.org/index.php?page=post&s=list";
@@ -29,13 +29,24 @@ public class WeatherForecastController : ControllerBase
         var node = doc.DocumentNode.SelectSingleNode("//*[@id=\"post-list\"]/div[2]");
         //son resimin urlsi
         var lastId = node?.ChildNodes[1]?.ChildNodes[0]?.ChildNodes[0]?.Attributes?.Where(f => f.Name == "href")?.FirstOrDefault()?.Value.Split("=").LastOrDefault();
-        var itemUrl = $"https://safebooru.org/index.php?page=post&s=view&id={lastId}";
-        //son resimin bilgileri
+        return lastId;
+    }
+
+    public async Task<String> getRandomImage()
+    {
+        var lastId = await maxId();
+        var integer = int.Parse(lastId);
+        var id = new Random(integer);
+        var itemUrl = $"https://safebooru.org/index.php?page=post&s=view&id={id}";
+        //resim bilgileri
+        var web = new HtmlAgilityPack.HtmlWeb();
         var itemPage = web.Load(itemUrl);
         var i = itemPage.DocumentNode.SelectSingleNode("//*[@id=\"image\"]");
-        Console.WriteLine(i.Attributes.Where(d => d.Name == "src").FirstOrDefault());
+
         var src = i?.Attributes?.Where(d => d.Name == "src")?.FirstOrDefault()?.Value.Split("?").First();
+
         var tagList = new List<string>();
+
         var sideBar = itemPage.DocumentNode.SelectSingleNode("//*[@id=\"tag-sidebar\"]");
         foreach (var child in sideBar.ChildNodes)
         {
@@ -44,8 +55,15 @@ public class WeatherForecastController : ControllerBase
                 tagList.Add(child.ChildNodes.Where(d => d.Name == "a").FirstOrDefault().FirstChild.GetDirectInnerText());
             }
         }
+
+        return Newtonsoft.Json.JsonConvert.SerializeObject(new { itemUrl, src, tagList });
+    }
+
+
+    public async Task<String?> GetMax()
+    {
         
-        return Newtonsoft.Json.JsonConvert.SerializeObject(new {itemUrl, src, tagList });
+        return await getRandomImage();
     }
 }
 
